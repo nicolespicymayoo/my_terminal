c22a90a297ec40b563b5bf4f31aa8e990d1bc13c
Nicole Mayo <nicolemayo@Nicoles-MBP.fios-router.home>
2023-04-24 14:33:46 -0400

    add icons for theme toggler. move css of terminal buttons

--- src/components/theme-toggler/ThemeToggler.tsx
+++ src/components/theme-toggler/ThemeToggler.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { DarkThemeIcon } from './DarkThemeIcon'
+import { LightThemeIcon } from './LightThemeIcon'
 import './ThemeToggler.scss'
+import {Theme} from 'App'
 
-export const ThemeToggler = (theme: 'dark' | 'light', toggleTheme: () => void) => {
-  return <div className='toggle-theme'>
-      <div className={`theme-option-${theme}`}>light</div>
-        <input className="checkbox" type="checkbox" id="switch" checked={theme === 'dark'}/>
-        <label className={`slider-${theme}`} htmlFor="switch" onClick={toggleTheme} />
-        <div className={`theme-option-${theme}`}>dark</div>
-      </div>
-}
+export const ThemeToggler = (theme: Theme, toggleTheme: () => void) => (
+  <div className='toggle-theme'>
+    <div><LightThemeIcon theme={theme} /></div>
+    <input className="checkbox" type="checkbox" id="switch" checked={theme === 'dark'}/>
+    <label className={`slider-${theme}`} htmlFor="switch" onClick={toggleTheme} />
+    <div><DarkThemeIcon theme={theme} /></div>
+  </div>
+)
